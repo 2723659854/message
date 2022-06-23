475b7a8d195c743c46858b54c175379d8d3b383e
yanglong <2723659854@qq.com>
2022-06-23 13:06:14 +0800

    第一次提交代码

---
+++ READEME.md
@@ -0,0 +1,18 @@
+##
+使用方法
+
+第一步、安装&nbsp;<br>
+composer require xiaosongshu/message
+第二步、引入message类<br>
+use Xiaosongshu/Message;<br>
+第三步，传入参数并实例化<br>
+$client=new Meessage(string $appId,string $appkey,string $mastersecret,string $baseUrl)<br>
+第四步、共有三个方法<br>
+单个推送： $client->send_one(array $param)<br>
+批量推送： $client->send_list(array $param)<br>
+全部推送： $client->send_all(array $param)<br>
+第五步、推送完成后返回数据<br>
+return ['code' => 200, 'msg' => ''', 'task_id' =>''];
+<br>
+##
+开发者联系方式：2723659854@qq.com
---
+++ src/Message.php
@@ -0,0 +1,478 @@
+<?php
+
+namespace Xiaosongshu\Appmessage;
+require __DIR__ . '/../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+class Message
+{
+
+    protected $appId        = '';
+    protected $appkey       = "";
+    protected $mastersecret = "";
+    protected $baseUrl      = "";
+
+    /**
+     * Message constructor.初始化实例
+     * @param string $appId appid 必填
+     * @param string $appkey appkey 必填
+     * @param string $mastersecret mstaersecret 必填
+     * @param string $baseUrl baseUrl 必填 个推接口地址
+     */
+    public function __construct(string $appId, string $appkey, string $mastersecret, string $baseUrl)
+    {
+        $this->appId        = $appId;
+        $this->appkey       = $appkey;
+        $this->mastersecret = $mastersecret;
+        $this->baseUrl      = $baseUrl;
+    }
+
+    /**
+     * 发送所有人
+     * @param array|string[] $param =[
+     *  'title'=>'',
+     *  'content'=>'',
+     *  'url'=>''
+     * ]
+     * @return array=[
+     *      'code'=>200,
+     *      'msg'=>'',
+     *      'task_id'=>[]
+     * ]
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function send_all(array $param = ['title' => '', 'content' => '', 'url' => '']): array
+    {
+        $token = $this->get_token();
+
+        $client1  = new Client();
+        $response = $client1->request(
+            'POST',
+            $this->baseUrl . '/push/all',
+            [
+                'headers' => [
+                    'content-type' => 'application/json;charset=utf-8',
+                    'token'        => $token
+                ],
+                'json'    => [//
+                              'request_id'   => $this->make_request_id(),
+                              'group_name'   => 'test',
+                              'audience'     => 'all',
+                              'settings'     => [
+                                  'ttl'      => 3600000,
+                                  'strategy' => [
+                                      'default' => 1,
+                                      'ios'     => 1,
+                                      'st'      => 1,
+                                      'hw'      => 1,
+                                      'vv'      => 1,
+                                      'op'      => 1,
+                                      'mz'      => 1,
+                                  ]
+
+                              ],
+                              'push_message' => [
+                                  'transmission' => json_encode(
+                                      [
+                                          'title'      => $param['title'],
+                                          'body'       => $param['content'],
+                                          'click_type' => 'payload',
+                                          'payload'    => $param['url'],
+                                      ]
+                                  ),
+
+                              ],
+                              'push_channel' => [
+                                  'android' => [
+                                      'ups' => [
+                                          'notification' => [
+                                              'title'      => $param['title'],
+                                              'body'       => $param['content'],
+                                              'click_type' => 'intent',
+                                              'intent'     => 'intent:#Intent;action=android.intent.action.oppopush;launchFlags=0x04000000;component=szedu.com.shenzhi.szedu/io.dcloud.PandoraEntry;S.UP-OL-SU=true;S.title=测试标题;S.content=测试内容;S.payload=' . $param['url'] . ';end'
+                                          ],
+
+                                      ]
+                                  ],
+                                  "ios"     => [
+                                      "type"       => "notify",
+                                      "payload"    => $param['url'],
+                                      "aps"        => [
+                                          "alert"             => [
+                                              "title" => $param['title'],
+                                              "body"  => $param['content'],
+                                          ],
+                                          "content-available" => 0,
+                                          "sound"             => "default",
+                                      ],
+                                      "auto_badge" => "+1",
+                                      'custom1'    => "other information",
+                                  ]
+                              ]
+                ],
+            ]
+        );
+        $code     = $response->getStatusCode();
+        if ($code == 200) {
+            $data = $response->getBody()->getContents();
+            $data = json_decode($data, true);
+            if (isset($data['data']['taskid'])) {
+                $task_id = $data['data']['taskid'];
+            } else {
+                $task_id = 0;
+            }
+            return ['code' => 200, 'msg' => $data['msg'], 'task_id' => [$task_id]];
+        } else {
+            return ['code' => 400, 'msg' => '消息发送失败！', 'task_id' => []];
+        }
+    }
+
+
+    /**
+     * 单设备推送
+     * @param array|string[] $param =[
+     *  'title'=>'',
+     *  'content'=>'',
+     *  'url'=>'',
+     *  'cid'=>'',
+     * ]
+     * @return array=[
+     *      'code'=>200,
+     *      'msg'=>'',
+     *      'task_id'=>[]
+     * ]
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function send_one(array $param = ['title' => '', 'content' => '', 'url' => '', 'cid' => '']): array
+    {
+        $token    = $this->get_token();
+        $client1  = new Client();
+        $response = $client1->request(
+            'POST',
+            $this->baseUrl . '/push/single/cid',
+            [
+                'headers' => [
+                    'content-type' => 'application/json;charset=utf-8',
+                    'token'        => $token
+                ],
+                'json'    => [
+                    'request_id'   => $this->make_request_id(),
+                    'group_name'   => 'test',
+                    'audience'     => [
+                        'cid' => [$param['cid']]
+                    ],
+                    'settings'     => [
+                        'ttl'      => 3600000,
+                        'strategy' => [
+                            'default' => 1,
+                            'ios'     => 1,
+                            'st'      => 1,
+                        ]
+
+                    ],
+                    'push_message' => [
+                        'transmission' => json_encode(
+                            [
+                                'title'      => $param['title'],
+                                'content'    => $param['content'],
+                                'click_type' => 'payload',
+                                'payload'    => $param['url']
+                            ]
+                        ),
+                    ],
+                    'push_channel' => [
+                        'android' => [
+                            'ups' => [
+                                'notification' => [
+                                    'title'      => $param['title'],
+                                    'body'       => $param['content'],
+                                    'click_type' => 'intent',
+                                    'intent'     => 'intent:#Intent;action=android.intent.action.oppopush;launchFlags=0x04000000;component=szedu.com.shenzhi.szedu/io.dcloud.PandoraEntry;S.UP-OL-SU=true;S.title=测试标题;S.content=测试内容;S.payload=' . $param['url'] . ';end'
+                                ],
+
+                            ]
+                        ],
+                        "ios"     => [
+                            "type"       => "notify",
+                            "payload"    => $param['url'],
+                            "aps"        => [
+                                "alert"             => [
+                                    "title" => $param['title'],
+                                    "body"  => $param['content'],
+                                ],
+                                "content-available" => 0,
+                                "sound"             => "default",
+                            ],
+                            "auto_badge" => "+1",
+                            'custom1'    => "other information",
+                        ]
+                    ]
+                ],
+
+            ]
+        );
+        $code     = $response->getStatusCode();
+
+        if ($code == 200) {
+            $data = $response->getBody()->getContents();
+            $data = json_decode($data, true);
+
+            if (isset($data['data']['taskid'])) {
+                $task_id = $data['data']['taskid'];
+            } else {
+                $task_id = 0;
+            }
+            return ['code' => 200, 'msg' => $data['msg'], 'task_id' => [$task_id]];
+        } else {
+            return ['code' => 400, 'msg' => '消息发送失败', 'task_id' => []];
+        }
+    }
+
+    /**
+     * 批量推送
+     * @param array $param =[
+     *  'title'=>'',
+     *  'content'=>'',
+     *  'url'=>'',
+     *  'cid'=>[],
+     * ]
+     * @return array=[
+     *      'code'=>200,
+     *      'msg'=>'',
+     *      'task_id'=>[]
+     * ]
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function send_list(array $param = ['title' => '', 'content' => '', 'url' => '', 'cids' => []]): array
+    {
+        $cids     = $param['cids'];
+        $count    = 0;
+        $task_ids = [];
+        foreach (array_chunk($cids, 500) as $k => $cid) {
+            $task_id = $this->get_task_id($param);
+            if (!$task_id) {
+                return ['code' => 400, 'msg' => '第' . ($count) . '次消息批量发送失败！失败原因：获取任务Id失败，任务终止。', 'task_id' => []];
+            }
+            $token    = $this->get_token();
+            $client1  = new Client();
+            $response = $client1->request(
+                'POST',
+                $this->baseUrl . '/push/list/cid',
+                [
+                    'headers' => [
+                        'content-type' => 'application/json;charset=utf-8',
+                        'token'        => $token
+                    ],
+                    'json'    => [
+                        'request_id'   => $this->make_request_id(),
+                        'taskid'       => $task_id,
+                        'group_name'   => 'test',
+                        'audience'     => [
+                            'cid' => $cid
+                        ],
+                        'settings'     => [
+                            'ttl'      => 3600000,
+                            'strategy' => [
+                                'default' => 1,
+                                'ios'     => 1,
+                                'st'      => 1,
+                                'hw'      => 1,
+                                'vv'      => 1,
+                                'op'      => 1,
+                                'mz'      => 1,
+                            ]
+
+                        ],
+                        'push_message' => [
+                            'transmission' => json_encode(
+                                [
+                                    'title'      => $param['title'],
+                                    'body'       => $param['content'],
+                                    'click_type' => 'payload',
+                                    'payload'    => $param['url'],
+                                ]
+                            ),
+
+                        ],
+                        'push_channel' => [
+                            'android' => [
+                                'ups' => [
+                                    'notification' => [
+                                        'title'      => $param['title'],
+                                        'body'       => $param['content'],
+                                        'click_type' => 'intent',
+                                        'intent'     => 'intent:#Intent;action=android.intent.action.oppopush;launchFlags=0x04000000;component=szedu.com.shenzhi.szedu/io.dcloud.PandoraEntry;S.UP-OL-SU=true;S.title=测试标题;S.content=测试内容;S.payload=' . $param['url'] . ';end'
+                                    ],
+
+                                ]
+                            ],
+                            "ios"     => [
+                                "type"       => "notify",
+                                "payload"    => $param['url'],
+                                "aps"        => [
+                                    "alert"             => [
+                                        "title" => $param['title'],
+                                        "body"  => $param['content'],
+                                    ],
+                                    "content-available" => 0,
+                                    "sound"             => "default",
+                                ],
+                                "auto_badge" => "+1",
+                                'custom1'    => "other information",
+                            ]
+                        ]
+                    ],
+                ]
+            );
+            $code     = $response->getStatusCode();
+            if ($code == 200) {
+                $data = $response->getBody()->getContents();
+                $data = json_decode($data, true);
+
+                if (isset($data['data']['taskid'])) {
+                    $task_id = $data['data']['taskid'];
+                } else {
+                    $task_id = 0;
+                }
+                $task_ids[] = $task_id;
+                $count++;
+            } else {
+                return ['code' => 400, 'msg' => '第' . ($count) . '次消息批量发送失败！失败原因：其它。任务终止。'];
+            }
+        }
+        return ['code' => 200, 'msg' => '分地区推送消息成功，共分为' . $count . '次推送完成！', 'task_id' => $task_ids];
+    }
+
+    protected function get_token()
+    {
+        $timestamp = $this->getMillisecond();
+        $client    = new Client();
+        $sign      = $this->sha256($this->appkey . $timestamp . $this->mastersecret);
+        $response  = $client->request(
+            'POST',
+            $this->baseUrl . '/auth',
+            [
+                'header' => [
+                    'content-type' => 'application/json;charset=utf-8',
+                ],
+                'json'   => [
+                    'sign'      => $sign,
+                    'timestamp' => $timestamp,
+                    'appkey'    => $this->appkey
+                ],
+
+            ]
+        );
+        $code      = $response->getStatusCode();
+        if ($code == 200) {
+            $data = $response->getBody()->getContents();
+            $data = json_decode($data, true);
+            return $data['data']['token'];
+        } else {
+            return '获取token失败';
+        }
+    }
+
+    protected function make_request_id()
+    {
+        return 'zlf' . rand(10000, 99999) . rand(100, 999);
+    }
+
+    protected function getMillisecond()
+    {
+        list($s1, $s2) = explode(' ', microtime());
+        return (float)sprintf('%.0f', (floatval($s1) + floatval($s2)) * 1000);
+    }
+
+    protected function get_task_id($param)
+    {
+        $token = $this->get_token();
+
+        $client1  = new Client();
+        $response = $client1->request(
+            'POST',
+            $this->baseUrl . '/push/list/message',
+            [
+
+                'headers' => [
+                    'content-type' => 'application/json;charset=utf-8',
+                    'token'        => $token
+                ],
+                'json'    => ([
+                    'request_id'   => $this->make_request_id(),
+                    'group_name'   => 'test',
+                    'is_async'     => false,
+                    'settings'     => [
+                        'ttl'      => 360,
+                        'strategy' => [
+                            'default' => 1,
+                            'ios'     => 1,
+                            'st'      => 1,
+                            'hw'      => 1,
+                            'vv'      => 1,
+                            'op'      => 1,
+                            'mz'      => 1,
+                        ]
+                    ],
+                    'push_message' => [
+                        'transmission' => json_encode(
+                            [
+                                'title'      => $param['title'],
+                                'body'       => $param['content'],
+                                'click_type' => 'payload',
+                                'payload'    => $param['url'],
+                            ]
+                        ),
+
+                    ],
+                    'push_channel' => [
+                        'android' => [
+                            'ups' => [
+                                'notification' => [
+                                    'title'      => $param['title'],
+                                    'body'       => $param['content'],
+                                    'click_type' => 'intent',
+                                    'intent'     => 'intent:#Intent;action=android.intent.action.oppopush;launchFlags=0x04000000;component=szedu.com.shenzhi.szedu/io.dcloud.PandoraEntry;S.UP-OL-SU=true;S.title=' . $param['title'] . ';S.content=' . $param['content'] . ';S.payload=' . $param['url'] . ';end'
+                                ],
+
+                            ]
+                        ],
+
+
+                        "ios" => [
+                            "type"       => "notify",
+                            "payload"    => $param['url'],
+                            "aps"        => [
+                                "alert"             => [
+                                    "title" => $param['title'],
+                                    "body"  => $param['content'],
+                                ],
+                                "content-available" => 0,
+                                "sound"             => "default",
+                            ],
+                            "auto_badge" => "+1",
+                            'custom1'    => "other information",
+                        ]
+
+
+                    ]
+                ]),
+            ]
+        );
+        $code     = $response->getStatusCode();
+        if ($code == 200) {
+            $data = $response->getBody()->getContents();
+            $data = json_decode($data, true);
+            return $data['data']['taskid'];
+        } else {
+            return 0;
+        }
+    }
+
+    protected function sha256($str)
+    {
+        return hash('sha256', $str);
+    }
+
+}
